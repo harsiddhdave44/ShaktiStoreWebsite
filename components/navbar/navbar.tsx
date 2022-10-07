85b66c3d88c3479ad3ffdca66935835fef63d193
Harsiddh Dave <harsiddhdave44@outlook.com>
2022-10-08 01:45:39 +0530

    Fixed collapsible view showing in large screens

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -30,8 +30,8 @@ export const Nav = () => {
          <Navbar.Brand>
             <Navbar.Toggle aria-label="toggle navigation" showIn="sm" />
             <AcmeLogo />
-            <Text b color="inherit" hideIn="xs">
-               ACME
+            <Text b color="inherit">
+               Shakti Store
             </Text>
             <Navbar.Content
                hideIn="sm"
@@ -39,86 +39,10 @@ export const Nav = () => {
                   pl: '6rem',
                }}
             >
-               {/* <Dropdown isBordered>
-                  <Navbar.Item>
-                     <Dropdown.Button
-                        auto
-                        light
-                        css={{
-                           px: 0,
-                           dflex: 'center',
-                           svg: {pe: 'none'},
-                        }}
-                        iconRight={icons.chevron}
-                        ripple={false}
-                     >
-                        Features
-                     </Dropdown.Button>
-                  </Navbar.Item>
-                  <Dropdown.Menu
-                     aria-label="ACME features"
-                     css={{
-                        '$$dropdownMenuWidth': '340px',
-                        '$$dropdownItemHeight': '70px',
-                        '& .nextui-dropdown-item': {
-                           'py': '$4',
-                           'svg': {
-                              color: '$secondary',
-                              mr: '$4',
-                           },
-                           '& .nextui-dropdown-item-content': {
-                              w: '100%',
-                              fontWeight: '$semibold',
-                           },
-                        },
-                     }}
-                  >
-                     <Dropdown.Item
-                        key="autoscaling"
-                        showFullDescription
-                        description="ACME scales apps to meet user demand, automagically, based on load."
-                        icon={icons.scale}
-                     >
-                        Autoscaling
-                     </Dropdown.Item>
-                     <Dropdown.Item
-                        key="usage_metrics"
-                        showFullDescription
-                        description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
-                        icon={icons.activity}
-                     >
-                        Usage Metrics
-                     </Dropdown.Item>
-                     <Dropdown.Item
-                        key="production_ready"
-                        showFullDescription
-                        description="ACME runs on ACME, join us and others serving requests at web scale."
-                        icon={icons.flash}
-                     >
-                        Production Ready
-                     </Dropdown.Item>
-                     <Dropdown.Item
-                        key="99_uptime"
-                        showFullDescription
-                        description="Applications stay on the grid with high availability and high uptime guarantees."
-                        icon={icons.server}
-                     >
-                        +99% Uptime
-                     </Dropdown.Item>
-                     <Dropdown.Item
-                        key="supreme_support"
-                        showFullDescription
-                        description="Overcome any challenge with a supporting team ready to respond."
-                        icon={icons.user}
-                     >
-                        +Supreme Support
-                     </Dropdown.Item>
-                  </Dropdown.Menu>
-               </Dropdown> */}
                <Navbar.Link isActive href="#">
                   Home
                </Navbar.Link>
-               <Navbar.Link isActive href="#">
+               <Navbar.Link href="#">
                   Products
                </Navbar.Link>
                <Navbar.Link href="#">About Us</Navbar.Link>
@@ -126,7 +50,7 @@ export const Nav = () => {
             </Navbar.Content>
          </Navbar.Brand>
 
-         <Navbar.Collapse>
+         <Navbar.Collapse showIn="sm">
             {collapseItems.map((item, index) => (
                <Navbar.CollapseItem key={item}>
                   <Link
@@ -150,7 +74,7 @@ export const Nav = () => {
             </Navbar.CollapseItem>
          </Navbar.Collapse>
          <Navbar.Content>
-            <Navbar.Item hideIn={'xs'}>
+            <Navbar.Item hideIn={'sm'}>
                <Switch
                   checked={isDark}
                   onChange={(e) =>
